50107bd0c2c14b86608ac2389ff99f91e240c90f
charlesmmarshall <charles.marshall@digital.justice.gov.uk>
2020-10-26 15:56:10 +0000

    Main call to trigger the detail command

---
+++ main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"opg-infra-costs/commands"
+	"os"
+)
+
+// startOfDay takes a time and generates a Date for the start of that day
+// so we can use Now() to get start time of day
+
+func main() {
+
+	detailCmd, _ := commands.DetailCommand()
+
+	switch os.Args[1] {
+	case detailCmd.Name:
+		commands.RunDetailCommand(detailCmd)
+	default:
+		fmt.Printf("Command details listed below.\n\n [%s]:\n", detailCmd.Name)
+		detailCmd.Set.PrintDefaults()
+		fmt.Println()
+		os.Exit(1)
+	}
+
+}
