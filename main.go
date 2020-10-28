fac4ff97b4fe38cf818ee0c7c97bb9eee5603eb5
charlesmmarshall <charles.marshall@digital.justice.gov.uk>
2020-10-28 12:02:12 +0000

    introduce the month to date command

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"opg-infra-costs/commands/detail"
+	"opg-infra-costs/commands/monthtodate"
 	"os"
 )
 
@@ -12,13 +13,19 @@ import (
 func main() {
 
 	detailCmd, _ := detail.Command()
+	mtdCmd, _ := monthtodate.Command()
 
 	switch os.Args[1] {
 	case detailCmd.Name:
 		detail.Run(detailCmd)
+	case mtdCmd.Name:
+		monthtodate.Run(mtdCmd)
 	default:
-		fmt.Printf("Command details listed below.\n\n [%s]:\n", detailCmd.Name)
+		fmt.Println("Commands listed below:")
+		fmt.Printf(" *%s*:\n", detailCmd.Name)
 		detailCmd.Set.PrintDefaults()
+		fmt.Printf(" *%s*:\n", mtdCmd.Name)
+		mtdCmd.Set.PrintDefaults()
 		fmt.Println()
 		os.Exit(1)
 	}
