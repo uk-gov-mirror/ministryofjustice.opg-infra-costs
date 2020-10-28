6675b7bcfc676880048f541572a21e0a191e6ad5
charlesmmarshall <charles.marshall@digital.justice.gov.uk>
2020-10-28 09:19:11 +0000

    Add helpers to the struct to allow the dynamic fetch based on string keys in data
    “Id”, “Account.Id” etc returns the correct string

--- accounts/account.go
+++ accounts/account.go
@@ -8,6 +8,22 @@ type Account struct {
 	Region      string `default:"eu-west-1"`
 }
 
+func (a Account) asMap() map[string]string {
+	return map[string]string{
+		"Id":          a.Id,
+		"Name":        a.Name,
+		"Environment": a.Environment,
+		"Role":        a.Role,
+		"Region":      a.Region,
+	}
+}
+
+// Arn returns the aws ARN formatted string
 func (a Account) Arn() string {
 	return "arn:aws:iam::" + a.Id + ":role/" + a.Role
+}
+
+// Get returns property
+func (a Account) Get(prop string) string {
+	return a.asMap()[prop]
 }
--- costs/structs.go
+++ costs/structs.go
@@ -1,12 +1,32 @@
 package costs
 
-import "opg-infra-costs/accounts"
+import (
+	"opg-infra-costs/accounts"
+	"strings"
+)
 
 type CostRow struct {
 	Date    string
 	Service string
 	Cost    string
 	Account accounts.Account
+}
+
+func (c CostRow) asMap() map[string]string {
+	return map[string]string{
+		"Date":    c.Date,
+		"Service": c.Service,
+		"Cost":    c.Cost,
+	}
+}
+
+// Get helper
+func (c CostRow) Get(prop string) string {
+	// pass down to account
+	if strings.Contains(prop, "Account.") {
+		return c.Account.Get(strings.ReplaceAll(prop, "Account.", ""))
+	}
+	return c.asMap()[prop]
 }
 
 type CostData struct {
