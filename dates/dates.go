e3304483991b34cbbbadbb1e19dc3acf784977b1
charlesmmarshall <charles.marshall@digital.justice.gov.uk>
2020-11-03 13:54:12 +0000

    YM date format

--- dates/dates.go
+++ dates/dates.go
@@ -12,6 +12,10 @@ func AWSDateFormat() string {
 	return "2006-01-02"
 }
 
+func AWSDateFormatYM() string {
+	return "2006-01"
+}
+
 func StartOfDay(t time.Time) time.Time {
 	year, month, day := t.Date()
 	return time.Date(year, month, day, 0, 0, 0, 0, t.UTC().Location())
