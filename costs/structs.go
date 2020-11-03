25686c62aed3a625b0652ff083c4f2d6751edd3e
charlesmmarshall <charles.marshall@digital.justice.gov.uk>
2020-11-03 13:54:34 +0000

    Meta property to allow some more flex on data for display

--- costs/structs.go
+++ costs/structs.go
@@ -11,6 +11,7 @@ type CostRow struct {
 	Service string
 	Cost    float64
 	Account accounts.Account
+	Meta    map[string]string
 }
 
 func (c *CostRow) asMap() map[string]string {
@@ -26,6 +27,9 @@ func (c *CostRow) Get(prop string) string {
 	// pass down to account
 	if strings.Contains(prop, "Account.") {
 		return c.Account.Get(strings.ReplaceAll(prop, "Account.", ""))
+	} else if strings.Contains(prop, "Meta.") {
+		meta := c.Meta
+		return meta[strings.ReplaceAll(prop, "Meta.", "")]
 	}
 	return c.asMap()[prop]
 }
