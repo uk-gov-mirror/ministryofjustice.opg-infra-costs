8ef91d158ccced40e9913bc0ca738f04ac96e6ea
charlesmmarshall <charles.marshall@digital.justice.gov.uk>
2020-11-03 13:55:10 +0000

    Move the map creation to own function for external use

--- costs/grouping.go
+++ costs/grouping.go
@@ -8,6 +8,15 @@ import (
 // GroupByKeys returns a new CostData re-organised to be grouped by the key array passed
 func (cd *CostData) GroupByKeys(keys []string) CostData {
 	costs := CostData{}
+	mapped := cd.GroupByKeysMap(keys)
+	// add to the costs - range does not give order
+	for _, row := range mapped {
+		costs.Entries = append(costs.Entries, row)
+	}
+	return costs
+}
+
+func (cd *CostData) GroupByKeysMap(keys []string) map[string]CostRow {
 	mapped := make(map[string]CostRow)
 	//loop over entries and group costs
 	for _, c := range cd.Entries {
@@ -19,17 +28,12 @@ func (cd *CostData) GroupByKeys(keys []string) CostData {
 		if found, ok = mapped[key]; ok {
 			foundFloat = found.Cost
 		}
-		// convert back to string
 		sum := foundFloat + c.Cost
 		found = c
 		found.Cost = sum
 		mapped[key] = found
 	}
-	// add to the costs - range does not give order
-	for _, row := range mapped {
-		costs.Entries = append(costs.Entries, row)
-	}
-	return costs
+	return mapped
 }
 
 // gorupKey generates a combined string to use as a single level key for maps
