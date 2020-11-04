2d48af5a67dea2808e0b7acc8f455aa2c48b81e2
charlesmmarshall <charles.marshall@digital.justice.gov.uk>
2020-11-04 11:57:33 +0000

    Date helpers

--- dates/dates.go
+++ dates/dates.go
@@ -4,27 +4,46 @@ import (
 	"time"
 )
 
+// DateFormat returns the date format to use
 func DateFormat() string {
 	return time.RFC3339
 }
 
+// AWSDateFormat returns YY-MM-DD format for use with AWS api
 func AWSDateFormat() string {
 	return "2006-01-02"
 }
 
+// AWSDateFormatYM returns YY-MM format for use with AWS api
 func AWSDateFormatYM() string {
 	return "2006-01"
 }
 
+//StartOfDay returns the times for the begining of the day (so zeroes hours / minutes)
 func StartOfDay(t time.Time) time.Time {
 	year, month, day := t.Date()
 	return time.Date(year, month, day, 0, 0, 0, 0, t.UTC().Location())
 }
 
+//DateStringToYearMonth converts a string to YY-MM-DD and then to YY-MM
 func DateStringToYearMonth(d string) (string, error) {
 	t, e := time.Parse(AWSDateFormat(), d)
 	if e != nil {
 		return "", e
 	}
-	return t.Format("2006-01"), nil
+	return t.Format(AWSDateFormatYM()), nil
+}
+
+//Months returns a slice of headers between 2 dates passed
+// - excludes the last month (2020-01 - 2020-11 would give you 2020-01 to 2020-10)
+func Months(
+	startDate time.Time,
+	endDate time.Time,
+	df string,
+) []time.Time {
+	months := []time.Time{}
+	for d := startDate; d.Format(df) < endDate.Format(df); d = d.AddDate(0, 1, 0) {
+		months = append(months, d)
+	}
+	return months
 }
