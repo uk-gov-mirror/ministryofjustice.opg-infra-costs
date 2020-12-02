b7998a17cc36e078dff43305958ef60f3e4e2c97
charlesmmarshall <charles.marshall@digital.justice.gov.uk>
2020-12-02 10:40:13 +0000

    Update the costs result handling to also operate in async fashion with a mutux lock to ensure append is safe

--- costs/async.go
+++ costs/async.go
@@ -2,14 +2,19 @@ package costs
 
 import (
 	"opg-infra-costs/accounts"
+	"sync"
 	"time"
 )
+
+type costResult struct {
+	Costs []CostRow
+	Error error
+}
 
 // getCosts does the outbound call to AWS - is used as a goroutine
 // - uses channels to pass data / errors back
 func getCosts(
-	out chan []CostRow,
-	err chan error,
+	channel chan costResult,
 	account accounts.Account,
 	start time.Time,
 	end time.Time,
@@ -17,13 +22,27 @@ func getCosts(
 	service string) {
 
 	data, e := Unblended(account, start, end, period, service)
+	t := costResult{Costs: data}
 	if e != nil {
-		err <- e
-		out <- nil
-	} else {
-		out <- data
-		err <- nil
+		t.Error = e
 	}
+	channel <- t
+}
+
+func gotCosts(
+	channel chan costResult,
+	waitgroup *sync.WaitGroup,
+	mu *sync.Mutex,
+	costsRows *[]CostRow) {
+
+	c := <-channel
+	if c.Error == nil {
+		mu.Lock()
+		*costsRows = append(*costsRows, c.Costs...)
+		mu.Unlock()
+	}
+	waitgroup.Done()
+
 }
 
 // AyncCosts calls the AWS api
@@ -37,23 +56,20 @@ func AsyncCosts(
 	service string) (CostData, error) {
 
 	var costData CostData
-	dataCh := make(chan []CostRow)
-	errorCh := make(chan error)
+	var rows []CostRow
+	var waitgroup sync.WaitGroup
+	mu := &sync.Mutex{}
+
+	channel := make(chan costResult)
 
 	for _, a := range *allAccounts {
-		go getCosts(dataCh, errorCh, a, startDate, endDate, period, service)
+		waitgroup.Add(1)
+		go getCosts(channel, a, startDate, endDate, period, service)
+		go gotCosts(channel, &waitgroup, mu, &rows)
 	}
-
-	for range *allAccounts {
-		c := <-dataCh
-		e := <-errorCh
-		if e != nil {
-			return costData, e
-		}
-		for _, r := range c {
-			costData.Entries = append(costData.Entries, r)
-		}
-	}
+	// wait till complete
+	waitgroup.Wait()
+	costData.Entries = rows
 
 	return costData, nil
 }
